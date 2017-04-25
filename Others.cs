879fb570d673787e389ff9af26974d7829e475bc
Kevin Pilch <me@pilchie.com>
2017-04-25 12:00:05 -0300

    Initial State

---
+++ Others.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Others
+{
+    async Task<int> ProcessAsync(object obj,
+        CancellationToken cancellationToken = default(CancellationToken))
+    {
+        await Task.Delay(TimeSpan.FromSeconds(1));
+
+        var s = obj as string;
+        if (s != null)
+        {
+            int i;
+            return int.TryParse(s, out i) ? i : -1;
+        }
+        else if (obj is int)
+        {
+            return (int)obj;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var numbers = new int[]
+        {
+            0x1, 0x2, 0x4, 0x8, 0x10, 0x20,
+        };
+    }
+}
