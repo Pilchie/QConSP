ee3968418d1a7875a803cc64773fdf6f7f79b41b
Kevin Pilch <me@pilchie.com>
2017-04-25 12:02:23 -0300

    Initial use of tuples

--- Program.cs
+++ Program.cs
@@ -9,5 +9,13 @@ class Program
         {
             0b1, 0b10, 0b100, 0b1000, 0b1_0000, 0b10_0000,
         };
+
+        var t = Tally(numbers);
+        WriteLine($"The count is {t.Item1}, and the sum is {t.Item2}.");
+    }
+
+    private static (int, int) Tally(int[] numbers)
+    {
+        throw new NotImplementedException();
     }
 }
