a85bef7dabffcf06bd6d78a87a9ca74f3408be17
Kevin Pilch <me@pilchie.com>
2017-04-25 12:10:28 -0300

    Convert to binary literals

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ class Program
     {
         var numbers = new int[]
         {
-            0x1, 0x2, 0x4, 0x8, 0x10, 0x20,
+            0b1, 0b10, 0b100, 0b1000, 0b1_0000, 0b10_0000,
         };
     }
 }
