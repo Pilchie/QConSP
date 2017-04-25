1171bc400982448ad7ab90d5b43119722db48e40
Kevin Pilch <me@pilchie.com>
2017-04-25 12:03:43 -0300

    Use deconstruction

--- Program.cs
+++ Program.cs
@@ -10,8 +10,8 @@ class Program
             0b1, 0b10, 0b100, 0b1000, 0b1_0000, 0b10_0000,
         };
 
-        var t = Tally(numbers);
-        WriteLine($"The count is {t.sum}, and the sum is {t.count}.");
+        var (sum, count) = Tally(numbers);
+        WriteLine($"The count is {sum}, and the sum is {count}.");
     }
 
     private static (int sum, int count) Tally(int[] numbers)
